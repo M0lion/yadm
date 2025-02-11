5041c77f720b93181ff7daf50d8c4ab88f55facc
Bjørn Moholt <bjorn@moholt.dev>
2025-02-11 22:08:36 +0100

    Adds tpm, cattpucin and Hack Font Mono

--- .bashrc_shared.sh
+++ .bashrc_shared.sh
@@ -1,3 +1,13 @@
 export PATH=$HOME/bin:$PATH
 
+if ! test -f ~/.local/bin/getnf; then
+	curl -fsSL https://raw.githubusercontent.com/getnf/getnf/main/install.sh | bash -s -- --silent
+fi
+
+# Check if Hack font is installed using fc-list
+if ! fc-list | grep -i "Hack Nerd Font Mono" > /dev/null; then
+    echo "Hack font not found. Installing..."
+    getnf -i "Hack Mono"
+fi
+
 alias tnux=tmux-projects.sh

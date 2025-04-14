dcc2911dbfadd9264176650dd42c6bc236fde6e5
Bjørn Moholt <bjorn@mewb.no>
2025-04-14 08:25:45 +0200

    Add blink.cmp completion plugin

---
+++ .config/nvim/lua/plugins/completion.lua
@@ -0,0 +1,40 @@
+return {
+  {
+    "blink-cmp/blink.cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/nvim-cmp",
+      "L3MON4D3/LuaSnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-d>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+        experimental = {
+          ghost_text = true,
+        },
+      })
+    end,
+  },
+}
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -4,6 +4,7 @@ return {
 	{ import = "plugins.lualine" },
 	{ import = "plugins.lsp" },
 	{ import = "plugins.ai" },
+	{ import = "plugins.completion" },
 	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 	{ "tpope/vim-fugitive", config = function() vim.keymap.set("n", "<leader>gs", vim.cmd.Git) end }
 }

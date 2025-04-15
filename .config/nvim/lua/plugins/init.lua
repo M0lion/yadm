42d66fbd83ec0b2a6cf43122ebb9bb46f6924002
Bjørn Moholt <bjorn@mewb.no>
2025-04-15 09:23:28 +0200

    Formatting

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -3,8 +3,8 @@ return {
 	{ import = "plugins.telescope" },
 	{ import = "plugins.lualine" },
 	{ import = "plugins.lsp" },
-	-- { import = "plugins.ai" },
 	{ import = "plugins.completion" },
+	{ import = "plugins.formatting" },
 	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 	{ "tpope/vim-fugitive", config = function() vim.keymap.set("n", "<leader>gs", vim.cmd.Git) end }
 }

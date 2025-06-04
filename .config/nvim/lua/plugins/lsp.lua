1a3ea90ba0ea5203d6ddb0e1c5cf67023160055b
Bjørn Moholt <bjorn@mewb.no>
2025-06-04 11:04:40 +0200

    Fixes lsp keybinds by removing on_attach

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -44,22 +44,19 @@ return {
 				automatic_enable = true,
 			})
 
-			local on_attach = function(_, bufnr)
-				local opts = { noremap = true, silent = true, buffer = bufnr }
-				-- LSP keybindings
-				vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-				vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
-				vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
-				vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, opts)
-				vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+			-- LSP keybindings
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+			vim.keymap.set("n", "K", vim.lsp.buf.hover)
+			vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename)
+			vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action)
+			vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+			vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end)
 
-				-- LSP-related Telescope keybindings
-				local builtin = require('telescope.builtin')
-				vim.keymap.set('n', '<leader>ps', builtin.lsp_document_symbols, opts)
-				vim.keymap.set('n', '<leader>per', builtin.diagnostics, opts)
-				vim.keymap.set('n', '<leader>prr', builtin.lsp_references, opts)
-			end
+			-- LSP-related Telescope keybindings
+			local builtin = require('telescope.builtin')
+			vim.keymap.set('n', '<leader>ps', builtin.lsp_document_symbols)
+			vim.keymap.set('n', '<leader>per', builtin.diagnostics)
+			vim.keymap.set('n', '<leader>prr', builtin.lsp_references)
 
 			-- Set up global LSP behavior
 			local capabilities = require('blink.cmp').get_lsp_capabilities()
